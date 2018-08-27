5d3161442f7327e7a390e66a642389fc3d1224ee
Steve Wagner <ciroque@live.com>
2018-08-27 12:03:29 -0700

    Minor tweaks

--- lib/trig/degrees.ex
+++ lib/trig/degrees.ex
@@ -12,10 +12,10 @@ defmodule Degrees do
     {:ok, %{degrees: decimal_degrees}}
   end
 
-  def to_degrees_minutes_seconds(%{degrees: degrees}) do
+  def to_degrees_minutes_seconds(%DecimalDegrees{degrees: degrees}) do
     whole_degrees = :erlang.floor(degrees)
     fractional_degrees = degrees - whole_degrees
-    minutes = fractional_degrees * (60 / 1)
+    minutes = fractional_degrees * 60
     whole_minutes = :erlang.floor(minutes)
     seconds = (minutes - whole_minutes) * 60
 
